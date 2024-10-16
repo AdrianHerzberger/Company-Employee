bbf15c03e2f395c5e82b31554a2d06f996876009
Adrian Herzberger <adrianherzberger94@gmail.com>
2024-10-16 16:33:41 +0200

    implement user authetication -remove log files from tracking

--- Services/IAuthenticationService.cs
+++ Services/IAuthenticationService.cs
@@ -11,5 +11,7 @@ namespace Service.Contracts
     public interface IAuthenticationService
     {
         Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto);
+        Task<bool> ValidateUser(UserForAuthenticationDto userForAuth);
+        Task<string> CreateToken();
     }
 }
